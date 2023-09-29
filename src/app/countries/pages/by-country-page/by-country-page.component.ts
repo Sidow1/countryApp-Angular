4781294e1458c11d178d2624b55ec83d22508efa
Thomas Chavez <thomas.chavezc@utem.cl>
2023-09-28 23:05:14 -0300

    Mejoras y optimizaciones implementadas

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
-import { Country } from '../../interfaces/country';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'countries-by-country-page',
   templateUrl: './by-country-page.component.html',
   styles: [],
 })
-export class ByCountryPageComponent {
+export class ByCountryPageComponent implements OnInit {
   public countries: Country[] = [];
+  public initialValue: string = '';
 
   constructor(private countryService: CountriesService) {}
+
+  ngOnInit(): void {
+    this.countries = this.countryService.cacheStore.byCountries.countries;
+    this.initialValue = this.countryService.cacheStore.byCountries.term;
+  }
 
   searchCountry(term: string): void {
     this.countryService.searchCountry(term).subscribe((countries) => {
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,3 +1,4 @@
+import { OnInit } from '@angular/core';
 import {
   Component,
   Input,
@@ -5,26 +6,52 @@ import {
   ElementRef,
   Output,
   EventEmitter,
+  OnDestroy,
 } from '@angular/core';
+import { Subject, debounceTime, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: [],
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
   // @ViewChild('txtInput')
   // public tagInput!: ElementRef<HTMLInputElement>;
+
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSubscription?: Subscription;
+
+  @Input()
+  public placeholder: string = '';
+
+  @Input()
+  public initialValue: string = '';
 
   @Output()
   // public onValue: EventEmitter<string> = new EventEmitter();
   public onValue = new EventEmitter<string>();
 
-  @Input()
-  public placeholder: string = '';
+  @Output()
+  public onDebounce = new EventEmitter<string>();
 
+  ngOnInit(): void {
+    this.debouncerSubscription = this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe((value) => {
+        this.onDebounce.emit(value);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSubscription?.unsubscribe();
+  }
   emitTag(tag: string): void {
     // this.onValue.emit(this.tagInput.nativeElement.value);
     this.onValue.emit(tag);
+  }
+
+  onKeyPress(searchTerm: string): void {
+    this.debouncer.next(searchTerm);
   }
 }
